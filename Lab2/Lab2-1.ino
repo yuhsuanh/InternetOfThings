53216897928f1973c079e956b0f740844a0d2547
Yu-Hsuan Huang <yuhsuan.huang.tw@gmail.com>
2020-02-08 23:30:02 -0500

    add circuit for lab2

--- Lab2/Lab2-1.ino
+++ Lab2/Lab2-1.ino
@@ -2,177 +2,99 @@
   *       Name: Yu-Hsuan Huang
   *       Date: January 16th
   *      
-  *       Lab 2 - Level 3     
+  *       Title: 321Maker Experiment #002 - RGB Blink
+  *       Description: This program will cycle different colors using the RGB LED on the 321Maker shield.
+  *       Tutorial:  http://321maker.com/tutorial/e002-rgb-blink
+  *       Revision Date: January 15, 2016
+  *       Licence: http://321maker.mit-license.org/
+  *       Requirements:  Arduino with 321Maker Shield
+  *       Instructions: The RGB LED is connected to a Pulse Width Modulation(PWM) pin. 
+  *       You can use analogWrite to control brightness.
   */
 int RGBRedPin = 9;    //The red RGB LED is connected pin 9 of the Arduino.
 int RGBGreenPin = 10; //The green RGB LED is connected pin 10 of the Arduino.
 int RGBBluePin = 11;  //The blue RGB LED is connected pin 11 of the Arduino.
+int waitTime = 2000; //Sets the amount of time to wait between color changes.
 
-void setup() {
+int delayTime = 3000;
+
+void setup() { //The Setup function runs once.
   pinMode(RGBRedPin, OUTPUT);    //Setup red RGB LED pin as an output pin.
   pinMode(RGBGreenPin, OUTPUT);  //Setup green RGB LED pin as an output pin.
   pinMode(RGBBluePin, OUTPUT);   //Setup blue RGB LED pin as an output pin.
 }
 
-void loop() {
-  //level 3
-  //1
-  digitalWrite(RGBBluePin, HIGH);
-  digitalWrite(RGBRedPin, HIGH);
-  digitalWrite(RGBGreenPin, HIGH);
-  delay(100);
-  digitalWrite(RGBGreenPin, LOW);
-  delay(100);
-  digitalWrite(RGBRedPin, HIGH);
-  delay(100);
-  digitalWrite(RGBBluePin, LOW);
-  digitalWrite(RGBRedPin, HIGH);
-  digitalWrite(RGBGreenPin, HIGH);
-  delay(100);
-  digitalWrite(RGBBluePin, HIGH);
-  digitalWrite(RGBRedPin, LOW);
-  delay(100);
-  digitalWrite(RGBGreenPin, LOW);
-  digitalWrite(RGBBluePin, LOW);
-  delay(100);
-  digitalWrite(RGBBluePin, HIGH);
-  delay(100);
-  digitalWrite(RGBRedPin, LOW);
-  delay(100);
-  digitalWrite(RGBBluePin, HIGH);
-  digitalWrite(RGBRedPin, HIGH);
-  digitalWrite(RGBGreenPin, HIGH);
-  delay(100);
-  digitalWrite(RGBBluePin, LOW);
-  digitalWrite(RGBRedPin, LOW);
-  digitalWrite(RGBGreenPin, LOW);
-  delay(100);
+void loop() { //The loop function runs forever.
+//  
+//  //Display Red
+//  digitalWrite(RGBRedPin, HIGH); //Turn on RED
+//  delay(waitTime);               //Wait for 2 seconds
+//  digitalWrite(RGBRedPin, LOW);  //Turn off Red
+//  
+//  //Display Green
+//  digitalWrite(RGBGreenPin, HIGH); //Turn on RED
+//  delay(waitTime);               //Wait for 2 seconds
+//  digitalWrite(RGBGreenPin, LOW);  //Turn off Red
+//  
+//  //Display Blue
+//  digitalWrite(RGBBluePin, HIGH); //Turn on RED
+//  delay(waitTime);               //Wait for 2 seconds
+//  digitalWrite(RGBBluePin, LOW);  //Turn off Red
+//  
+//  //Display  Magenta  (Red + Blue)
+//  digitalWrite(RGBRedPin, HIGH); //Turn on RED
+//  digitalWrite(RGBBluePin, HIGH); //Turn on RED
+//  delay(waitTime);               //Wait for 2 seconds
+//  digitalWrite(RGBRedPin, LOW);  //Turn off Red
+//  digitalWrite(RGBBluePin, LOW); //Turn on RED
+//  
+//  //Display  Yellow  (Red + Green)
+//  digitalWrite(RGBRedPin, HIGH); //Turn on RED
+//  digitalWrite(RGBGreenPin, HIGH); //Turn on RED
+//  delay(waitTime);               //Wait for 2 seconds
+//  digitalWrite(RGBRedPin, LOW);  //Turn off Red
+//  digitalWrite(RGBGreenPin, LOW); //Turn of RED
+//  
+//  //Display  Cyan  (Blue + Green) 
+//  digitalWrite(RGBBluePin, HIGH); //Turn on BLUE
+//  digitalWrite(RGBGreenPin, HIGH); //Turn on GREEN
+//  delay(waitTime);               //Wait for 2 seconds
+//  digitalWrite(RGBBluePin, LOW); //Turn of BLUE
+//  digitalWrite(RGBGreenPin, LOW);  //Turn off GREEN
+//  
+//  //Display White  (Red + Blue + Green)
+//  digitalWrite(RGBRedPin, HIGH); //Turn on RED
+//  digitalWrite(RGBBluePin, HIGH); //Turn on BLUE
+//  digitalWrite(RGBGreenPin, HIGH); //Turn on GREEN
+//  delay(waitTime);               //Wait for 2 seconds
+//  digitalWrite(RGBRedPin, LOW);  //Turn off RED
+//  digitalWrite(RGBBluePin, LOW); //Turn off BLUE
+//  digitalWrite(RGBGreenPin, LOW); //Turn off GREEN
 
-  //2
-  digitalWrite(RGBGreenPin, HIGH);
-  delay(200);
-  digitalWrite(RGBRedPin, HIGH);
-  delay(200);
-  digitalWrite(RGBGreenPin, LOW);
+  //level 2-1 & 2-2
+  //Blue Light
   digitalWrite(RGBBluePin, HIGH);
-  delay(200);
-  digitalWrite(RGBRedPin, LOW);
+  delay(delayTime);
+  digitalWrite(RGBBluePin, LOW);
+  delay(delayTime);
+  //White Light
+  digitalWrite(RGBBluePin, HIGH);
+  digitalWrite(RGBRedPin, HIGH);
   digitalWrite(RGBGreenPin, HIGH);
-  delay(200);
+  delay(delayTime);
   digitalWrite(RGBBluePin, LOW);
   digitalWrite(RGBRedPin, LOW);
   digitalWrite(RGBGreenPin, LOW);
-  delay(200);
-
-  //3
-  digitalWrite(RGBBluePin, HIGH);
+  delay(delayTime);
+  //Red Light
   digitalWrite(RGBRedPin, HIGH);
-  digitalWrite(RGBGreenPin, HIGH);
-  delay(500);
-  digitalWrite(RGBBluePin, HIGH);
+  delay(delayTime);
   digitalWrite(RGBRedPin, LOW);
+  delay(delayTime);
+  //Blue Light
   digitalWrite(RGBGreenPin, HIGH);
-  delay(500);
-  //4
-  digitalWrite(RGBBluePin, LOW);
-  digitalWrite(RGBRedPin, LOW);
-  digitalWrite(RGBGreenPin, HIGH);
-  delay(500);
-  digitalWrite(RGBBluePin, LOW);
-  digitalWrite(RGBRedPin, HIGH);
-  digitalWrite(RGBGreenPin, HIGH);
-  delay(500);
-  //5
-  digitalWrite(RGBBluePin, HIGH);
-  digitalWrite(RGBRedPin, LOW);
+  delay(delayTime);
   digitalWrite(RGBGreenPin, LOW);
-  delay(500);
-  digitalWrite(RGBBluePin, HIGH);
-  digitalWrite(RGBRedPin, HIGH);
-  digitalWrite(RGBGreenPin, LOW);
-  delay(500);
-  //6
-  digitalWrite(RGBBluePin, HIGH);
-  digitalWrite(RGBRedPin, HIGH);
-  digitalWrite(RGBGreenPin, LOW);
-  delay(500);
-  digitalWrite(RGBBluePin, LOW);
-  digitalWrite(RGBRedPin, HIGH);
-  digitalWrite(RGBGreenPin, LOW);
-  delay(500);
-  //7
-  digitalWrite(RGBBluePin, HIGH);
-  digitalWrite(RGBRedPin, HIGH);
-  digitalWrite(RGBGreenPin, HIGH);
-  delay(500);
-  digitalWrite(RGBBluePin, LOW);
-  digitalWrite(RGBRedPin, LOW);
-  digitalWrite(RGBGreenPin, LOW);
-  delay(500);
-
-  //8
-  digitalWrite(RGBRedPin, HIGH);
-  delay(200);
-  digitalWrite(RGBBluePin, HIGH);
-  delay(200);
-  digitalWrite(RGBRedPin, LOW);
-  digitalWrite(RGBGreenPin, HIGH);
-  delay(200);
-  digitalWrite(RGBRedPin, HIGH);
-  delay(200);
-  digitalWrite(RGBBluePin, LOW);
-  digitalWrite(RGBRedPin, LOW);
-  digitalWrite(RGBGreenPin, LOW);
-  delay(2000);
-  
-  //9-14
-  digitalWrite(RGBRedPin, HIGH);
-  delay(1000);
-  digitalWrite(RGBGreenPin, HIGH);
-  delay(1000);
-  digitalWrite(RGBRedPin, LOW);
-  digitalWrite(RGBGreenPin, LOW);
-  digitalWrite(RGBBluePin, HIGH);
-  delay(1000);
-  digitalWrite(RGBRedPin, HIGH);
-  delay(1000);
-  digitalWrite(RGBGreenPin, HIGH);
-  digitalWrite(RGBBluePin, LOW);
-  delay(1000);
-  digitalWrite(RGBRedPin, LOW);
-  digitalWrite(RGBGreenPin, LOW);
-  delay(1000);
-  
-  //15
-  digitalWrite(RGBBluePin, HIGH);
-  digitalWrite(RGBRedPin, HIGH);
-  digitalWrite(RGBGreenPin, HIGH);
-  delay(100);
-  digitalWrite(RGBGreenPin, LOW);
-  delay(100);
-  digitalWrite(RGBRedPin, HIGH);
-  delay(100);
-  digitalWrite(RGBBluePin, LOW);
-  digitalWrite(RGBRedPin, HIGH);
-  digitalWrite(RGBGreenPin, HIGH);
-  delay(100);
-  digitalWrite(RGBBluePin, HIGH);
-  digitalWrite(RGBRedPin, LOW);
-  delay(100);
-  digitalWrite(RGBGreenPin, LOW);
-  digitalWrite(RGBBluePin, LOW);
-  delay(100);
-  digitalWrite(RGBBluePin, HIGH);
-  delay(100);
-  digitalWrite(RGBRedPin, LOW);
-  delay(100);
-  digitalWrite(RGBBluePin, HIGH);
-  digitalWrite(RGBRedPin, HIGH);
-  digitalWrite(RGBGreenPin, HIGH);
-  delay(100);
-  digitalWrite(RGBBluePin, LOW);
-  digitalWrite(RGBRedPin, LOW);
-  digitalWrite(RGBGreenPin, LOW);
-  delay(100);
+  delay(delayTime);
   
 }
